6239d7ce32f7c98df608ac57e5401b1f49a3c3b8
nirasan <nirasan@gmail.com>
2016-09-26 09:12:50 +0900

    wip

---
+++ toml.go
@@ -0,0 +1,264 @@
+package toml
+
+import (
+	"fmt"
+	"github.com/pelletier/go-toml"
+	"go/ast"
+	"reflect"
+	"unicode"
+
+	"errors"
+)
+
+func Load(v interface{}, file string, env string) error {
+	tree, err := toml.LoadFile(file)
+	if err != nil {
+		return err
+	}
+
+	if v == nil {
+		return fmt.Errorf("v must not be nil")
+	}
+	rv := reflect.ValueOf(v)
+	if rv.Kind() != reflect.Ptr || rv.Elem().Kind() != reflect.Struct {
+		return fmt.Errorf("v must be a struct pointer")
+	}
+
+	rv = rv.Elem()
+	rt := rv.Type()
+
+	for i := 0; i < rt.NumField(); i++ {
+		ft := rt.Field(i)
+		fv := rv.Field(i)
+		if !ast.IsExported(ft.Name) {
+			continue
+		}
+		name := getFieldName(ft)
+		value, err := getValue(fv.Type(), tree, name, env)
+		if err != nil {
+			return err
+		}
+		fv.Set(value)
+	}
+	return nil
+}
+
+//func getData(t reflect.Type, tree *toml.TomlTree, path, elem, env string) reflect.Value {
+//	envPath := createPath(path, env, elem)
+//	elemPath := createPath(path, elem)
+//	switch t.Kind() {
+//	case reflect.Struct:
+//		rv := reflect.New(t)
+//		for i := 0; i < t.NumField(); i++ {
+//			fv := rv.Field(i)
+//			ft := t.Field(i)
+//			if !ast.IsExported(ft.Name) {
+//				continue
+//			}
+//			name := getFieldName(ft)
+//			rv.Field(i).Set(getData(fv, tree, elemPath, name, env))
+//		}
+//		return rv
+//	case reflect.Array, reflect.Slice:
+//		rv := reflect.New(t).Elem()
+//		p := elemPath
+//		if tree.Has(envPath) {
+//			p = envPath
+//		}
+//		node := tree.Get(p)
+//		switch val := node.(type) {
+//		// for primitive array
+//		case []interface{}:
+//			for _, el := range val {
+//				rv = reflect.Append(rv, reflect.ValueOf(el))
+//			}
+//		// for map of array
+//		case []*toml.TomlTree:
+//			for i, childTree := range val {
+//				value := getData(t.Elem(), childTree, "", , env)
+//				log.Printf("%T %v", value, value)
+//				rv = reflect.Append(rv, value)
+//			}
+//		default:
+//		}
+//		return rv
+//	default:
+//		if tree.Has(envPath) {
+//			return reflect.ValueOf(tree.Get(envPath))
+//		} else if tree.Has(elemPath) {
+//			return reflect.ValueOf(tree.Get(elemPath))
+//		}
+//	}
+//	return reflect.ValueOf(nil)
+//}
+
+var nilValue = reflect.ValueOf(nil)
+
+func getValue(t reflect.Type, tree *toml.TomlTree, elem, env string) (reflect.Value, error) {
+	switch t.Kind() {
+	case reflect.Struct:
+		return getStructValue(t, tree, elem, env)
+	case reflect.Map:
+		return getMapValue(t, tree, elem, env)
+	case reflect.Array, reflect.Slice:
+		return getArrayValue(t, tree, elem, env)
+	default:
+		return getBasicValue(t, tree, elem, env)
+	}
+}
+
+func getBasicValue(t reflect.Type, tree *toml.TomlTree, elem, env string) (reflect.Value, error) {
+	p, err := findPath(tree, elem, env)
+	if err != nil {
+		return nilValue, err
+	}
+	v := tree.Get(p)
+	if reflect.TypeOf(v) != t {
+		return nilValue, errors.New("invalid type")
+	}
+	return reflect.ValueOf(v), nil
+}
+
+func getArrayValue(t reflect.Type, tree *toml.TomlTree, elem, env string) (reflect.Value, error) {
+	if t.Kind() != reflect.Array || t.Kind() != reflect.Slice {
+		return nilValue, errors.New("invalid type")
+	}
+	p, err := findPath(tree, elem, env)
+	if err != nil {
+		return nilValue, err
+	}
+	v := tree.Get(p)
+	et := t.Elem()
+	rv := reflect.New(t)
+	switch ary := v.(type) {
+	case []*toml.TomlTree:
+		for _, childTree := range ary {
+			ev, e := getValue(et, childTree, "", elem)
+			if e != nil {
+				return nilValue, e
+			}
+			reflect.Append(rv, ev)
+		}
+	case []interface{}:
+		for _, a := range ary {
+			reflect.Append(rv, reflect.ValueOf(a))
+		}
+	default:
+		return nilValue, errors.New("Invalid type")
+	}
+	return rv, nil
+}
+
+func getMapValue(t reflect.Type, tree *toml.TomlTree, elem, env string) (reflect.Value, error) {
+	if t.Kind() != reflect.Map {
+		return nilValue, errors.New("invalid type")
+	}
+	target := tree
+	// find env if elem defined
+	if elem != "" {
+		p, err := findPath(tree, elem, env)
+		if err != nil {
+			return nilValue, err
+		}
+		newTree := tree.Get(p)
+		switch val := newTree.(type) {
+		case *toml.TomlTree:
+			target = val
+		default:
+			return nilValue, errors.New("invalud tree")
+		}
+	}
+	// get map value from tree
+	rv := reflect.New(t)
+	for _, k := range target.Keys() {
+		kv := reflect.ValueOf(k)
+		rv.SetMapIndex(kv, reflect.ValueOf(target.Get(k)))
+	}
+	return rv, nil
+}
+
+func getStructValue(t reflect.Type, tree *toml.TomlTree, elem, env string) (reflect.Value, error) {
+	if t.Kind() != reflect.Struct {
+		return nilValue, errors.New("invalid type")
+	}
+	target := tree
+	// find env if elem defined
+	if elem != "" {
+		p, err := findPath(tree, elem, env)
+		if err != nil {
+			return nilValue, err
+		}
+		newTree := tree.Get(p)
+		switch val := newTree.(type) {
+		case *toml.TomlTree:
+			target = val
+		default:
+			return nilValue, errors.New("invalid tree")
+		}
+	}
+	// get struct value from tree
+	rv := reflect.New(t)
+	for i := 0; i < t.NumField(); i++ {
+		fv := rv.Field(i)
+		ft := t.Field(i)
+		if !ast.IsExported(ft.Name) {
+			continue
+		}
+		name := getFieldName(ft)
+		value, err := getValue(ft.Type, target, name, env)
+		if err != nil {
+			return nilValue, err
+		}
+		fv.Set(value)
+	}
+	return rv, nil
+}
+
+func getFieldName(f reflect.StructField) string {
+	name := f.Tag.Get("toml")
+	if name == "" {
+		name = toSnake(f.Name)
+	}
+	return name
+}
+
+func toSnake(in string) string {
+	runes := []rune(in)
+	l := len(runes)
+
+	var out []rune
+	for i := 0; i < l; i++ {
+		if i > 0 && unicode.IsUpper(runes[i]) && ((i+1 < l && unicode.IsLower(runes[i+1])) || unicode.IsLower(runes[i-1])) {
+			out = append(out, '_')
+		}
+		out = append(out, unicode.ToLower(runes[i]))
+	}
+
+	return string(out)
+}
+
+func createPath(in ...string) string {
+	out := ""
+	for _, s := range in {
+		if s != "" {
+			if out == "" {
+				out = s
+			} else {
+				out = out + "." + s
+			}
+		}
+	}
+	return out
+}
+
+func findPath(tree *toml.TomlTree, elem, env string) (string, error) {
+	envPath := createPath(env, elem)
+	elemPath := createPath(elem)
+	if tree.Has(envPath) {
+		return envPath, nil
+	} else if tree.Has(elemPath) {
+		return elemPath, nil
+	} else {
+		return "", errors.New("path not found")
+	}
+}
---
+++ toml_test.go
@@ -0,0 +1,218 @@
+package toml
+
+import (
+	"github.com/pelletier/go-toml"
+	"reflect"
+	"testing"
+	"fmt"
+	"log"
+)
+
+//type Config struct {
+//	User          string
+//	Password      string
+//	MaxConnection int64
+//	Timeout       float64
+//	ShowSlowQuery bool
+//	//Addresses     []string
+//	Postgres      Postgres `toml:postgres`
+//}
+//
+//type Postgres struct {
+//	User     string `toml:user`
+//	Password string `toml:password`
+//}
+//
+//func TestLoad(t *testing.T) {
+//	c := &Config{}
+//	e := Load(c, "test/config.toml", "development")
+//	if e != nil {
+//		t.Error(e)
+//	}
+//	log.Println(c)
+//}
+
+func TestGetValue_basic(t *testing.T) {
+	tree, e := toml.Load(`
+	user = "admin"
+	pass = "admin"
+	[development]
+	user = "root"
+	`)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	stringType := reflect.TypeOf("")
+
+	// development
+	v, e := getValue(stringType, tree, "user", "development")
+	if e != nil {
+		t.Fatal(e)
+	}
+	if v.Interface().(string) != "root" {
+		fmt.Errorf("failed to load user data: %v", v)
+	}
+
+	// production
+	v, e = getValue(stringType, tree, "user", "production")
+	if e != nil {
+		t.Fatal(e)
+	}
+	if v.Interface().(string) != "admin" {
+		fmt.Errorf("failed to load user data: %v", v)
+	}
+}
+
+func TestGetValue_struct(t *testing.T) {
+	tree, e := toml.Load(`
+	[database]
+	user = "admin"
+	pass = "admin"
+	[database.development]
+	user = "root"
+	`)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	type Conf struct {
+		User string
+		Pass string
+	}
+
+	structType := reflect.TypeOf(Conf{})
+
+	// development
+	v, e := getValue(structType, tree, "database", "development")
+	if e != nil {
+		t.Fatal(e)
+	}
+	vi, ok := v.Interface().(Conf)
+	if !ok || vi.User != "root" {
+		fmt.Errorf("failed to load user data: %v", v)
+	}
+	log.Println(v)
+
+	// production
+	v, e = getValue(structType, tree, "database", "production")
+	if e != nil {
+		t.Fatal(e)
+	}
+	vi, ok = v.Interface().(Conf)
+	if !ok || vi.User != "root" {
+		fmt.Errorf("failed to load user data: %v", v)
+	}
+	log.Println(v)
+
+}
+
+/*
+func TestLoadData_struct(t *testing.T) {
+	tree, e := toml.Load(`
+	[database]
+	user = "admin"
+	pass = "admin"
+	[database.development]
+	user = "root"
+	`)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	type Database struct {
+		User string
+		Pass string
+	}
+
+	type Conf struct {
+		Database Database
+	}
+
+	c := &Conf{}
+
+	loadData(getFieldValue(c, "Database", "User"), tree, "database", "user", "development")
+	if c.Database.User != "root" {
+		log.Println(c)
+		t.Error("failed to load user data")
+	}
+
+	loadData(getFieldValue(c, "Database", "User"), tree, "database", "user", "production")
+	if c.Database.User != "admin" {
+		log.Println(c)
+		t.Error("failed to load user data")
+	}
+}
+
+func TestLoadData_array(t *testing.T) {
+	tree, e := toml.Load(`
+	users = ["admin", "user1"]
+	[development]
+	users = ["root"]
+	`)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	type Conf struct {
+		Users []string
+	}
+
+	c := &Conf{}
+
+	loadData(getFieldValue(c, "Users"), tree, "", "users", "development")
+	if len(c.Users) != 1 || c.Users[0] != "root" {
+		log.Println(c)
+		t.Error("failed to load user data")
+	}
+
+	loadData(getFieldValue(c, "Users"), tree, "", "users", "production")
+	if len(c.Users) != 2 || c.Users[0] != "admin" {
+		log.Println(c)
+		t.Error("failed to load user data")
+	}
+}
+
+func TestLoadData_arraystruct(t *testing.T) {
+	tree, e := toml.Load(`
+	[[databases]]
+	user = "user1"
+	pass = "pass1"
+
+	[[databases]]
+	user = "user2"
+	pass = "pass2"
+
+	[[databases.development]]
+	user = "userdev"
+	pass = "passdev"
+	`)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	type Database struct {
+		User string
+		Pass string
+	}
+
+	type Conf struct {
+		Databases []Database
+	}
+
+	c := &Conf{}
+
+	loadData(getFieldValue(c, "Databases"), tree, "", "databases", "development")
+	if len(c.Databases) != 1 || c.Databases[0].User != "userdev" {
+		log.Println("arraystruct:", c)
+		t.Error("failed to load user data")
+	}
+
+	loadData(getFieldValue(c, "Databases"), tree, "", "databases", "production")
+	if len(c.Databases) != 2 || c.Databases[0].User != "user1" {
+		log.Println("arraystruct:"+
+			"", c)
+		t.Error("failed to load user data")
+	}
+}
+*/
