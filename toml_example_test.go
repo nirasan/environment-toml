42670306f4171c28154a7bdcf699dcbb6edc3126
nirasan <niranirao@gmail.com>
2016-09-27 15:20:58 +0900

    add readme and example

--- toml_example_test.go
+++ toml_example_test.go
@@ -7,35 +7,29 @@ import (
 	"time"
 )
 
-type Example1Config struct {
-	Int1    int64
-	Float1  float64
-	String1 string
-	Bool1   bool
-	Date1   time.Time
-	Array1  []int64
-}
-
-/*
-# test/example1.toml
-int1 = 1
-float1 = 0.1
-string1 = "string 1"
-bool1 = true
-date1 = 1980-01-01T00:00:00Z
-array1 = [1, 2, 3]
-
-[development]
-int1 = 2
-
-[production]
-float1 = 0.5
-array1 = [4, 5]
-*/
-
 func ExampleLoad_example1development() {
-	c := &Example1Config{}
+	type Config struct {
+		Int1    int64
+		Float1  float64
+		String1 string
+		Bool1   bool
+		Date1   time.Time
+		Array1  []int64
+	}
 
+	/*
+		int1 = 1
+		float1 = 0.1
+		string1 = "string 1"
+		bool1 = true
+		date1 = 1980-01-01T00:00:00Z
+		array1 = [1, 2, 3]
+
+		[development]
+		int1 = 2
+	*/
+
+	c := &Config{}
 
 	err := toml.Load(c, "test/example1.toml", "development")
 	if err != nil {
@@ -50,7 +44,29 @@ func ExampleLoad_example1development() {
 }
 
 func ExampleLoad_example1production() {
-	c := &Example1Config{}
+	type Config struct {
+		Int1    int64
+		Float1  float64
+		String1 string
+		Bool1   bool
+		Date1   time.Time
+		Array1  []int64
+	}
+
+	/*
+		int1 = 1
+		float1 = 0.1
+		string1 = "string 1"
+		bool1 = true
+		date1 = 1980-01-01T00:00:00Z
+		array1 = [1, 2, 3]
+
+		[production]
+		float1 = 0.5
+		array1 = [4, 5]
+	*/
+
+	c := &Config{}
 
 	err := toml.Load(c, "test/example1.toml", "production")
 	if err != nil {
@@ -64,5 +80,69 @@ func ExampleLoad_example1production() {
 	// &{Int1:1 Float1:0.5 String1:string 1 Bool1:true Date1:1980-01-01 00:00:00 +0000 UTC Array1:[4 5]}
 }
 
+func ExampleLoad_example2development() {
+	type User struct {
+		Name string
+		Age  int64
+	}
 
+	type Config struct {
+		User User
+	}
 
+	/*
+		[user]
+		name = "user1"
+		age = 10
+
+		[user.development]
+		name = "user2"
+	*/
+
+	c := &Config{}
+
+	err := toml.Load(c, "test/example2.toml", "development")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// `Name` is overwritten
+	fmt.Printf("%+v", c)
+
+	// Output:
+	// &{User:{Name:user2 Age:10}}
+}
+
+func ExampleLoad_example2production() {
+	type User struct {
+		Name string
+		Age  int64
+	}
+
+	type Config struct {
+		User User
+	}
+
+	/*
+		[user]
+		name = "user1"
+		age = 10
+
+		[user.production]
+		name = "user3"
+		age = 20
+	*/
+
+	c := &Config{}
+
+	err := toml.Load(c, "test/example2.toml", "production")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// `Name` and `Age` is overwritten
+	fmt.Printf("%+v", c)
+
+	// Output:
+	// &{User:{Name:user3 Age:20}}
+}
